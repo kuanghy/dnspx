6aca7496ded698a5d602e4de6e26ec7742490453
Huayong Kuang <sudohuoty@163.com>
2020-09-15 20:59:15 +0800

    update version

--- dnspx/version.py
+++ dnspx/version.py
@@ -3,7 +3,7 @@
 # Copyright (c) Huoty, All rights reserved
 # Author: Huoty <sudohuoty@163.com>
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
 _VersionInfo = __import__("collections").namedtuple(
