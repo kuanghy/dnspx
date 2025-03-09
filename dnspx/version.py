51259d8fd2250e7319ec3eb083e041ef5c352293
Huayong Kuang <sudohuoty@163.com>
2025-03-09 22:36:27 +0800

    change version to 0.1.13

--- dnspx/version.py
+++ dnspx/version.py
@@ -3,7 +3,7 @@
 # Copyright (c) Huoty, All rights reserved
 # Author: Huoty <sudohuoty@163.com>
 
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 
 _VersionInfo = __import__("collections").namedtuple(
