92205f47a4c2560231d07c65d1c1aaa3b779cc54
Huayong Kuang <sudohuoty@163.com>
2026-01-24 00:02:07 +0800

    change version to 0.2.0

--- dnspx/version.py
+++ dnspx/version.py
@@ -3,7 +3,7 @@
 # Copyright (c) Huoty, All rights reserved
 # Author: Huoty <sudohuoty@163.com>
 
-__version__ = '0.1.15'
+__version__ = '0.2.0'
 
 
 _VersionInfo = __import__("collections").namedtuple(
